62a8a5c8ce70ad63eee0fbcff95c8dc8befc3938
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-04-10 23:57:56 -0300

    feat: implementa a estruta de rotas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import  morgan from 'morgan';
-
+import routes from './routes.js';
 export const app = express();
+
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(routes);
