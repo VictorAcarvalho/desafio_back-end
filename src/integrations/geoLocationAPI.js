1ab20f04708909064b2c520eb284172d626a6c16
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-04-10 23:18:05 -0300

    feat: implementa a integracao com a api de geolocalizacao

---
+++ src/integrations/geoLocationAPI.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+class GoogleMapsIntegration {
+    async getMapAddress(address) {
+      const mapsResponse = await this.getMapsResponse(address);
+      const formattedAddress = this.getFormattedAddress(mapsResponse);
+      const geoPoints = this.getGeopoint(mapsResponse);
+  
+      return {
+        address: formattedAddress,
+        location: geoPoints,
+      };
+    }
+  
+    getGeopoint(mapResponse) {
+      return mapResponse.geometry.location;
+    }
+  
+    getFormattedAddress(mapResponse) {
+      return mapResponse.formatted_address;
+    }
+  
+    async getMapsResponse(address) {
+      try {
+        const url = new URL(
+          `${process.env.GMAPS}?address=${address}&key=${process.env.GEO_KEY}`
+        );
+        const datapoints = await axios.get(url.toString());
+        return datapoints.data.results[0];
+      } catch (error) {
+        console.error(`Erro ao acessar a API do Google Maps: ${error.message}`);
+      }
+    }
+  }
+
+  export const googleMapsIntegration = new GoogleMapsIntegration();
